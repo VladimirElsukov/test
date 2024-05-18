fa88a2773d49e780af054cb0831b88e1ff5b8a35
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-05-18 14:59:24 +0300

    Создали функцию count_subarray_occurrences для подсчета колличества вхождений arr2 в arr1

--- checking_array.py
+++ checking_array.py
@@ -2,3 +2,12 @@ def is_subarray_equal(arr1, arr2, i):
 
     return arr1[i:i + len(arr2)] == arr2
 
+def count_subarray_occurrences(arr1, arr2):
+    count = 0
+
+    for i in range(len(arr1) - len(arr2) + 1):
+        if is_subarray_equal(arr1, arr2, i):
+            count += 1
+    return count
+
+
